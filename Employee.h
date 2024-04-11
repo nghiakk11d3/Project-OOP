236a6bd4328fe563cc18cf5de6bf7e1b6737b9e2
nghiakk11d3 <163376051+nghiakk11d3@users.noreply.github.com>
2024-04-11 21:14:59 +0700

    Add files via upload

--- Employee.h
+++ Employee.h
@@ -12,29 +12,32 @@ using namespace std;
 class Employee :public UserName {
 public:
 	float wage;
-	double Salary(time_t t_batdau, time_t t_ketthuc);
-
+	
 	int returnEmployee(Employee list_employee[], int numberEmployee, char searchAcc[]);
 	void InputEmp();
 	void addEmployee(Employee list_employee[], int& numberEmployee);
 	void print1Emp(Employee employee, int x, int y, int i);
 	void printListEmp(Employee list_employee[], int number);
+	void menuEmployee(Employee list_employee[], int& numberEmployee);
+	void editEmployee(Employee list_employee[], int &numberEmployee);
+	void deleteEmployee(Employee list_employee[], int& numberEmployee);
+	void statusReturn1line1(int numProduct)
+	{
+		char Return[] = { "Nhan [ENTER] De Quay Quay Lai" };
+		int lengthRe = strlen(Return);
+		gotoXY((172 - lengthRe) / 2 + 1, 9 + numProduct * 2);
+		cout << Return;
+		_getch();
+	}
 };
-double Employee::Salary(time_t t_batdau, time_t t_ketthuc) {
-	// Tính toán số giờ làm việc
-	double gio_lamviec = difftime(t_ketthuc, t_batdau) / 3600.0;
 
-	// Tính toán lương tổng
-	double luong_tong = gio_lamviec * 35000;
 
-	return luong_tong;
-}
 int Employee::returnEmployee(Employee list_employee[], int numberEmployee, char searchID[])
 {
 
 	for (int i = 0; i < numberEmployee; i++)
 	{
-		if (strcmp(list_employee[i].getID(), searchID) == 0)
+		if (strcmp(list_employee[i].ID, searchID) == 0)
 		{
 			return i;
 		}
@@ -46,12 +49,7 @@ void Employee::InputEmp() {
 	int x1 = 90;
 	int high = 16;
 	int width = 25;
-	char name[20];  
-	char phone[11]; 
-	char mail[15]; 
-	char id[10]; 
-	char shift[2]; 
-	char day[10];
+	
 	ShowCur(1);
 	HopKTen(x, y, width, high, 11, 1);
 	HopKTen(x + 26, y, width, high, 11, 1);
@@ -59,34 +57,30 @@ void Employee::InputEmp() {
 	gotoXY(x0, y + 1);
 	cout << "Ma Nhan Vien:";
 	gotoXY(x1, y + 1);
-	cin >>id;
-	UserName::setID(id);
+	cin >>this->ID;
 	gotoXY(x0, y + 3);
 	cout << "Ten Nhan Vien:";
 	gotoXY(x1, y + 3);
 	cin.ignore();
-	cin.getline(name, 20);
-UserName::setName(name);
+	cin.getline(this->NameUser, 20);
 	gotoXY(x0, y + 5);
 	cout << "So Dien Thoai:";
 	gotoXY(x1, y + 5);
-	cin >> (phone);
-UserName::setPhone(phone);
+	cin >> (this->PhoneUser);
 	gotoXY(x0, y + 7);
 	cout << "Mail:";
 	gotoXY(x1, y + 7);
-	cin >> (mail);
-UserName::setMail(mail);
+	cin >> (this->MailUser);
 	gotoXY(x0, y + 9);
 	cout << "Ca Lam:";
 	gotoXY(x1, y + 9);
-	cin >> (shift);
-UserName::setShift(shift);
+	cin >> (this->Shift);
+
 	gotoXY(x0, y + 11);
 	cout << "Ngay Lam: ";
 	gotoXY(x1, y + 11);
-	cin >> (day);
-UserName::setDay(day);
+	cin >> (this->Day);
+	cin.ignore();
 	
 
 }
@@ -108,41 +102,114 @@ void Employee::addEmployee(Employee list_employee[], int& numberEmployee)
 	char status[][40] = { "Da Them Thanh Cong", "Nhan [ENTER] De Quay Lai" };
 	statusreturnMenu(status, 45);
 	_getch();
-	return;
-	/*menuProduct(list_product, number);*/
+	
+	menuEmployee(list_employee, numberEmployee);
 }
 void Employee::print1Emp(Employee employee, int x, int y, int i) {
 	int x_width = 1;
 	gotoXY(x + x_width, y + 2 * i + 1);
-	cout << employee.getID();
+	cout << employee.ID;
 	x_width += lengEmp[0];
 	gotoXY(x + x_width, y + 2 * i + 1);
-	cout << employee.getNameUser();
+	cout << employee.NameUser;
 	x_width += lengEmp[1];
 	gotoXY(x + x_width, y + 2 * i + 1);
-	cout << employee.getPhoneUser();
+	cout << employee.PhoneUser;
 	x_width += lengEmp[2];
 	gotoXY(x + x_width, y + 2 * i + 1);
-	cout << employee.getMailUser();
+	cout << employee.MailUser;
 	x_width += lengEmp[3];
 	gotoXY(x + x_width, y + 2 * i + 1);
-	cout << employee.getShift();
+	cout << employee.Shift;
 	x_width += lengEmp[4];
 	gotoXY(x + x_width, y + 2 * i + 1);
-	cout << employee.getID();
+	cout << employee.Day;
 }
 void Employee::printListEmp(Employee list_employee[], int number)
 {
 	TextColor(1);
 	system("cls");
-	TaoBangHeader(number, 1, 1, "DANH SACH SAN PHAM", lengEmp, HeaderEmp, 6);
+	TaoBangHeader(number, 37, 0, "DANH SACH SAN PHAM", lengEmp, HeaderEmp, 6);
 	SetColor(7);
 	y += 4;
 	for (int i = 0; i < number; i++)
 	{
-		print1Emp(list_employee[i],1, 5, i);
+		print1Emp(list_employee[i],37, 4, i);
 	}
-	/*statusReturn1line(number - 2);
-	menuProduct(list_product, number);*/
+	statusReturn1line1(number - 2);
+	menuEmployee(list_employee, number);
+
 }
+void Employee::editEmployee(Employee list_employee[], int &numberEmployee) {
+	
+		TextColor(1);
+		system("cls");
+		char nameHead[][40] = { "Sua Thong Tin Nhan Vien ", "Nhap Ma Nhan Vien: " };
+		char searchID[10];
+		bool KT = false;
+		int Index = -1;
+		taomenuYeuCau(nameHead, searchID);
+		for (int i = 0; i < numberEmployee; i++)
+		{
+			if (strcmp(searchID, list_employee[i].ID) == 0)
+			{
+				list_employee[i].InputEmp();
+				KT = true;
+			}
+		}
+		if (KT == true)
+		{
+			char status[][40] = { "Da Sua Thanh Cong", "Nhan [ENTER] De Quay Lai" };
+			statusreturnMenu(status, 45);
+			_getch();
+			menuEmployee(list_employee, numberEmployee);
+		}
+		else
+		{
+			char status[][40] = { "Khong Tim Thay Ma", "Nhan [ENTER] De Quay Lai" };
+			statusreturnMenu(status, 45);
+			_getch();
+			menuEmployee(list_employee, numberEmployee);
+		}
+	}
+void Employee::deleteEmployee(Employee list_employee[], int& numberEmployee) {
+	TextColor(1);
+	system("cls");
+	char nameHead[][40] = { "Xoa Thong Tin Nhan Vien ", "Nhap Ma Nhan Vien: " };
+	char searchID[10];
+	bool KT = false;
+	int Index = -1;
+	taomenuYeuCau(nameHead, searchID);
+	for (int i = 0; i < numberEmployee; i++)
+	{
+		if (strcmp(searchID, list_employee[i].ID) == 0)
+		{
+			for (int j = i; j < numberEmployee - 1; j++) {
+				list_employee[j] = list_employee[j + 1];
+			}
+			numberEmployee--;
+			KT = true;
+			i--;
+			
+		}
+		
+	}
+	if (KT == true)
+	{
+		char status[][40] = { "Da Xoa Thanh Cong", "Nhan [ENTER] De Quay Lai" };
+		statusreturnMenu(status, 45);
+		_getch();
+		menuEmployee(list_employee, numberEmployee);
+	}
+	else
+	{
+		char status[][40] = { "Khong Tim Thay Ma", "Nhan [ENTER] De Quay Lai" };
+		statusreturnMenu(status, 45);
+		_getch();
+		menuEmployee(list_employee, numberEmployee);
+	}
+}
+
+
+
 #endif
--- UserName.h
+++ UserName.h
@@ -5,64 +5,13 @@
 #include <cstring>
 
 class UserName {
-private:
+public:
     char NameUser[20];
     char PhoneUser[11];
-    char MailUser[15];
-    char ID[10];
-    char Shift[2];
-    char Day[10];
-
-public:
-  
-    void setName(char name[]) {
-        strncpy(NameUser, name, sizeof(NameUser) - 1);
-        NameUser[sizeof(NameUser) - 1] = '\0'; // Đảm bảo kết thúc chuỗi
-    }
-    void setPhone(char phone[]) {
-        strncpy(PhoneUser, phone, sizeof(PhoneUser) - 1);
-        PhoneUser[sizeof(PhoneUser) - 1] = '\0';
-    }
-    void setMail(char mail[]) {
-        strncpy(MailUser, mail, sizeof(MailUser) - 1);
-        MailUser[sizeof(MailUser) - 1] = '\0';
-    }
-    void setID(char id[]) {
-        strncpy(ID, id, sizeof(ID) - 1);
-        ID[sizeof(ID) - 1] = '\0';
-    }
-    void setShift(char shift[]) {
-        strncpy(Shift, shift, sizeof(Shift) - 1);
-        Shift[sizeof(Shift) - 1] = '\0';
-    }
-    void setDay(char day[]) {
-        strncpy(Day, day, sizeof(Day) - 1);
-        Day[sizeof(Day) - 1] = '\0';
-    }
-    char* getNameUser() {
-        return NameUser;
-    }
-
-    char* getPhoneUser() {
-        return PhoneUser;
-    }
-
-    char* getMailUser() {
-        return MailUser;
-    }
-
-    char* getID() {
-        return ID;
-    }
-
-    char* getShift() {
-        return Shift;
-    }
-    char* getDay() {
-        return Day;
-    }
-
-   
+    char MailUser[30];
+    char ID[15];
+    char Shift[15];
+    char Day[15];      
 };
 
 #endif
